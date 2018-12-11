8cc60c8949b9440274ce2de1c52c5850d22e4156
Hayley K <mrs.kornilenko@gmail.com>
2018-12-10 23:16:44 -0500

    React Setup

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container } from 'reactstrap';
+
+class AppNavbar extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">Make It Happen </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/resources">Resources</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/copperco/">GitHub</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar
